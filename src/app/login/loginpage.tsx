cb895225a5469b61b0a232d59fe57757161e3d3b
Jerson Kyle Bonifacio <143814993+JisuKayl@users.noreply.github.com>
2024-08-30 11:50:16 +0800

    Hide/Unhide Password

--- src/app/login/loginpage.tsx
+++ src/app/login/loginpage.tsx
@@ -3,10 +3,18 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
+  const [isPassVisibilityOff, setIsPassVisibilityOff] = useState(false);
+
+  const togglePassVisibility = () => {
+    setIsPassVisibilityOff(!isPassVisibilityOff);
+  };
 
   const handleLogin = (e: any) => {
     if (!email || !password) {
@@ -39,19 +47,43 @@ export default function Login() {
       </div>
       <div className="flex flex-col items-center mb-6 space-y-4 mt-6">
         <input
-          className="placeholder-gray-200 rounded-full p-3 border-solid text-gray-800 mt-6 border-gray-200 w-64"
+          className="placeholder-gray-200 rounded-full p-3 px-5 border-solid text-gray-800 mt-6 border-gray-200 w-64"
           type="text"
           placeholder="Email Address"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <input
-          className="placeholder-gray-200 rounded-full p-3 border-solid text-slate-800 mt-2 border-gray-200 w-64"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+        {isPassVisibilityOff == false ? (
+          <div className="relative cursor-pointer">
+            <input
+              className="placeholder-gray-200 rounded-full p-3 pl-5 pr-12 border-solid text-slate-800 mt-2 border-gray-200 w-64"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <VisibilityOffIcon
+              className="absolute top-4 right-4"
+              sx={{ color: "gray", width: 25, height: 25 }}
+              onClick={togglePassVisibility}
+            />
+          </div>
+        ) : (
+          <div className="relative cursor-pointer">
+            <input
+              className="placeholder-gray-200 rounded-full p-3 pl-5 pr-12 border-solid text-slate-800 mt-2 border-gray-200 w-64"
+              type="text"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <VisibilityIcon
+              className="absolute top-4 right-4"
+              sx={{ color: "gray", width: 25, height: 25 }}
+              onClick={togglePassVisibility}
+            />
+          </div>
+        )}
         <Link href="/chats">
           <button
             className="w-64 bg-red-600 text-white rounded-full mt-2 p-3 border-none cursor-pointer"
